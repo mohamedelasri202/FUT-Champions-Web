1f012e120b2afb08832a59e08f361e4d844261fd
mohamedelasri202 <el7994995@gmail.com>
2024-11-26 02:40:48 +0100

    l'interface

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-blue': '#1e3a8a',
+          'custom-cyan': '#00FFFF',
+          'custom-purple': '#8A2BE2',
+        },
+      },
+    },
+  };
+  
